cef8fd37abca70612983af6363444b02f49ec225
EC2 Default User <ec2-user@ip-172-31-11-134.ap-northeast-1.compute.internal>
2022-08-17 11:04:29 +0000

    [add]meshiterro10-23

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
   root to:'homes#top'
   devise_for :users
-
-  # ７つのルーティングを一括生成(今回は下の３つだけ +createも)
-  resources :post_images, only: [:new, :create, :index, :show]
-
   get '/homes/about' => 'homes#about', as:'about'
+
+  # ７つのルーティングを一括生成(今回は下の5つだけ)
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+  # ７つのルーティングを一括生成(今回は下の3つだけ)
+  resources :users, only: [:show, :edit, :update]
+
+
 
 
 end
